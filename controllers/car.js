0e4822dd1191360b4d51fcfb000bfe8fc3118c12
chryzcode <alabaolanrewaju13@gmail.com>
2024-03-22 11:30:20 +0100

    push

---
+++ controllers/car.js
@@ -0,0 +1,4 @@
+import { carType, houseType } from "../models/car.js";
+import cloudinary from "cloudinary";
+import { StatusCodes } from "http-status-codes";
+import { User } from "../models/user.js";
---
+++ models/car.js
@@ -0,0 +1,74 @@
+import mongoose from "mongoose";
+import { Media } from "./house.js";
+
+const carTypeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a valid name"],
+  },
+});
+
+const carSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    carType: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "carType",
+      required: [true, "Please provide car type"],
+    },
+    brand: {
+      type: String,
+      required: [true, "Please provide brand"],
+    },
+    year: {
+      type: Number,
+      required: [true, "Please provide car year"],
+    },
+    color: {
+      type: String,
+      required: [true, "Please provide car color"],
+    },
+    plateNumber: {
+      type: String,
+      required: [true, "Please provide car plate number"],
+    },
+    dateAvailable: {
+      type: Date,
+      required: [true, "Please provide date"],
+    },
+    media: [Media],
+    description: {
+      type: String,
+      required: [true, "Please provide description"],
+    },
+    currency: {
+      type: String,
+      enum: ["USD"],
+      default: "USD",
+      required: [true, "Please provide currency, ex. USD"],
+    },
+    installmentalPayment: {
+      type: Boolean,
+      default: true,
+    },
+    negotiation: {
+      type: Boolean,
+      default: true,
+    },
+
+    features: [
+      {
+        type: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Car = mongoose.model("Car", carSchema);
+const carType = mongoose.model("carType", carTypeSchema);
+
+export { Car, carType };
