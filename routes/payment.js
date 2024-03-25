70535fc86695cf4bd6a984d75ce8dce1e7cd3d97
chryzcode <alabaolanrewaju13@gmail.com>
2024-03-25 10:17:53 +0100

    push

--- routes/payment.js
+++ routes/payment.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { successfulPayment, walletPayout, cancelPayment } from "../controllers/payment.js";
+import { successfulPayment, walletPayout, cancelPayment, getMonthlyIncome } from "../controllers/payment.js";
 import authenticateUser from "../middleware/authentication.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.route("/:bookingId/success").get(authenticateUser, successfulPayment);
 router.route("/:bookingId/cancel").get(authenticateUser, cancelPayment);
 router.route("/:bankId/payout").post(authenticateUser, walletPayout);
+router.route("/monthly/income").get(authenticateUser, getMonthlyIncome);
 
 export default router;
