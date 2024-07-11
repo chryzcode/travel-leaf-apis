7215cef2fee7e633aa595a0a6c39c038c9e4edfa
Blaspace <115324109+Blaspace@users.noreply.github.com>
2024-07-11 03:52:28 -0800

    worked on the multer middleware

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -9,16 +9,21 @@ export default async (req, res, next) => {
     throw new UnauthenticatedError("Authentication invalid");
   }
   const token = authHeader.split(" ")[1];
-  const user = await User.findOne({ token: token, verified: true });
-  if (user) {
-    try {
-      const payload = jwt.verify(token, process.env.JWT_SECRET);
-      // attach the user to the job routes
-      req.user = { userId: payload.userId, firstName: payload.firstName };
+  const user = await User.findOne({ token: token });
 
-      next();
-    } catch (error) {
-      throw new UnauthenticatedError("Authentication invalid");
+  if (user) {
+    if (!user.verified) {
+      try {
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+        // attach the user to the job routes
+        req.user = { userId: payload.userId, firstName: payload.firstName };
+
+        next();
+      } catch (error) {
+        throw new UnauthenticatedError("Authentication invalid");
+      }
+    } else {
+      res.status(403);
     }
   } else {
     throw new UnauthenticatedError("Authentication invalid");
