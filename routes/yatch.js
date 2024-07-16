f70d081e3da3f13960e7668708c7fce9dc66f3c5
James Ngbede Samuel <115324109+Blaspace@users.noreply.github.com>
2024-07-16 07:00:46 +0100

    Update yatch.js

--- routes/yatch.js
+++ routes/yatch.js
@@ -1,5 +1,6 @@
 import express from "express";
-import upload from "../middleware/multer.js";
+//import upload from "../middleware/multer.js";
+const upload = require("../middleware/multer.js")
 import {
   createYatch,
   editYatch,
